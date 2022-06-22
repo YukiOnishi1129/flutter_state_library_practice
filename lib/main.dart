909f033f1b927deec356d97591eccc1e5eac0b6f
yuki <yuki.onishi1129@gmail.com>
2022-06-23 00:42:41 +0900

    Providerパターンの状態管理

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,5 @@
-import 'dart:async';
-
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 void main() {
   runApp(const MyApp());
@@ -16,92 +15,133 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(),
     );
   }
 }
 
-// StatefulWidget
-// StatefulWidgetをextendsしたclassと、State<>を継承したStateClassの2つで構成される
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key, required this.title}) : super(key: key);
+class MyHomePage extends StatelessWidget {
+  const MyHomePage({Key? key}) : super(key: key);
 
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => MyHomePageState();
-}
-
-// クラス名: 先頭に「_」がついてるとprivateになる
-class MyHomePageState extends State<MyHomePage> {
-  //StatefulWidgetは一つのclassで、状態、ロジック、UIを定義できる
-  //子供のclassにロジック、状態を渡していける。 (Reactのpropみたいな感じ)
-  //子供はStateLessWidget
-  //子供のWidgetが多くなりそこに状態やロジックを渡していくことで、階層が深くなり管理がしにくくなるのがデメリット (Reactのバケツリレーと一緒の課題)
-
-  //State classで状態を定義
-  //状態
-  // int counter = 0;
-  //
-  // //ロジック
-  // void incrementCounter() {
-  //   setState(() {
-  //     counter++;
-  //   });
-  // }
-
-  // BLoCパターン: stateとロジックを別の処理で管理する
-  late MyHomePageLogic myHomePageLogic;
-
-  @override
-  void initState() {
-    super.initState();
-    myHomePageLogic = MyHomePageLogic();
-  }
-
-  //見た目 (UI)
   @override
   Widget build(BuildContext context) {
-    print('MyHomePageStateをビルド');
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            // constで定義することで、再描画しなくて良いことを指定する
-            const WidgetA(),
-            WidgetB(myHomePageLogic),
-            WidgetC(myHomePageLogic),
-          ],
+    print('MyHomePageをビルド');
+    return ChangeNotifierProvider(
+      create: (context) => MyHomePageState(),
+      child: Scaffold(
+        appBar: AppBar(
+          title: const Text('Flutterラボ'),
+        ),
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: const <Widget>[
+              // constで定義することで、再描画しなくて良いことを指定する
+              WidgetA(),
+              WidgetB(),
+              WidgetC(),
+            ],
+          ),
         ),
       ),
     );
   }
 }
 
-// BLoCパターン
-class MyHomePageLogic {
-  MyHomePageLogic() {
-    _counterController.sink.add(_counter);
-  }
-
-  final StreamController<int> _counterController = StreamController();
-  int _counter = 0;
-
-  Stream<int> get count => _counterController.stream;
+class MyHomePageState extends ChangeNotifier {
+  int counter = 0;
 
   void increment() {
-    _counter++;
-    _counterController.sink.add(_counter);
+    counter++;
+    notifyListeners();
   }
 
-  void dispose() {
-    _counterController.close();
+  void reBuild() {
+    notifyListeners();
   }
 }
+
+// StatefulWidget
+// StatefulWidgetをextendsしたclassと、State<>を継承したStateClassの2つで構成される
+// class MyHomePage extends StatefulWidget {
+//   const MyHomePage({Key? key, required this.title}) : super(key: key);
+//
+//   final String title;
+//
+//   @override
+//   State<MyHomePage> createState() => MyHomePageState();
+// }
+
+// クラス名: 先頭に「_」がついてるとprivateになる
+// class MyHomePageState extends State<MyHomePage> {
+//   //StatefulWidgetは一つのclassで、状態、ロジック、UIを定義できる
+//   //子供のclassにロジック、状態を渡していける。 (Reactのpropみたいな感じ)
+//   //子供はStateLessWidget
+//   //子供のWidgetが多くなりそこに状態やロジックを渡していくことで、階層が深くなり管理がしにくくなるのがデメリット (Reactのバケツリレーと一緒の課題)
+//
+//   //State classで状態を定義
+//   //状態
+//   // int counter = 0;
+//   //
+//   // //ロジック
+//   // void incrementCounter() {
+//   //   setState(() {
+//   //     counter++;
+//   //   });
+//   // }
+//
+//   // BLoCパターン: stateとロジックを別の処理で管理する
+//   late MyHomePageLogic myHomePageLogic;
+//
+//   @override
+//   void initState() {
+//     super.initState();
+//     myHomePageLogic = MyHomePageLogic();
+//   }
+//
+//   //見た目 (UI)
+//   @override
+//   Widget build(BuildContext context) {
+//     print('MyHomePageStateをビルド');
+//     return Scaffold(
+//       appBar: AppBar(
+//         title: Text(widget.title),
+//       ),
+//       body: Center(
+//         child: Column(
+//           mainAxisAlignment: MainAxisAlignment.center,
+//           children: <Widget>[
+//             // constで定義することで、再描画しなくて良いことを指定する
+//             const WidgetA(),
+//             WidgetB(myHomePageLogic),
+//             WidgetC(myHomePageLogic),
+//           ],
+//         ),
+//       ),
+//     );
+//   }
+// }
+
+// BLoCパターン
+// class MyHomePageLogic {
+//   MyHomePageLogic() {
+//     _counterController.sink.add(_counter);
+//   }
+//
+//   final StreamController<int> _counterController = StreamController();
+//   int _counter = 0;
+//
+//   Stream<int> get count => _counterController.stream;
+//
+//   void increment() {
+//     _counter++;
+//     _counterController.sink.add(_counter);
+//   }
+//
+//   void dispose() {
+//     _counterController.close();
+//   }
+// }
 
 // class MyHomePageInheritedWidget extends InheritedWidget {
 //   const MyHomePageInheritedWidget(
@@ -151,40 +191,37 @@ class WidgetA extends StatelessWidget {
 class WidgetB extends StatelessWidget {
   // 親のWidgetから値を渡せるように
   //constructorに定義
-  const WidgetB(this.myHomePageLogic, {Key? key}) : super(key: key);
+  const WidgetB({Key? key}) : super(key: key);
   // 渡される値を定義
-
-  final MyHomePageLogic myHomePageLogic;
 
   @override
   Widget build(BuildContext context) {
+    print('WidgetBをビルド');
+    // final int counter = context.watch<MyHomePageState>().counter;
+    final int counter =
+        context.select<MyHomePageState, int>((state) => state.counter);
+
     //InheritedWidgetで状態を取得する方法
     // final MyHomePageState state = MyHomePageInheritedWidget.of(context);
-    return StreamBuilder<int>(
-        stream: myHomePageLogic.count,
-        builder: (context, snapshot) {
-          print('WidgetBをビルド');
-          return Text(
-            '${snapshot.data}',
-            style: Theme.of(context).textTheme.headline4,
-          );
-        });
+    return Text(
+      '${counter}',
+      style: Theme.of(context).textTheme.headline4,
+    );
   }
 }
 
 class WidgetC extends StatelessWidget {
-  const WidgetC(this.myHomePageLogic, {Key? key}) : super(key: key);
-
-  final MyHomePageLogic myHomePageLogic;
+  const WidgetC({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     print('WidgetCをビルド');
+    final Function increment = context.read<MyHomePageState>().increment;
     // final MyHomePageState state =
     //     MyHomePageInheritedWidget.of(context, listen: false);
     return ElevatedButton(
       onPressed: () {
-        myHomePageLogic.increment();
+        increment();
       },
       child: const Text('カウント'),
     );
